5692cef9a0c168810e3792f787fc7732987f4f6c
User <tamerlankha@code.edu.az>
2022-06-02 18:52:25 +0400

    Add project files.

---
+++ hometask02.06.2022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hometask02._06._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User us = new User();
+            us.Email = "tamerlan@mail.ru";
+            us.FullName = "Tamerlan Alimardanli";
+            us.Password = "asdada";
+            us.ShowInfo();
+            Console.WriteLine(us.PasswordChecker());
+        }
+    }
+    public interface IAccaunt 
+    {
+        public abstract bool PasswordChecker();
+        public abstract void ShowInfo();
+    
+    }
+}
---
+++ hometask02.06.2022/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask02._06._2022
+{
+    class User:IAccaunt
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public  bool PasswordChecker( ) 
+        {
+            int digitcoun = 0;
+            int charUpCoun = 0;
+            int charLoCoun = 0;
+            for (int i = 0; i < Password.Length; i++)
+            {
+                if (Password.Length > 7)
+                {
+                    if (char.IsLower(Password[i]))
+                    {
+
+                        charLoCoun++;
+
+                    }
+                    else if (char.IsUpper(Password[i]))
+                    {
+                        charUpCoun++;
+                    }
+                    else if (char.IsDigit(Password[i]))
+                    {
+                        digitcoun++;
+
+                    }
+                    if (charLoCoun > 0 && charUpCoun > 0 && digitcoun > 0) 
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+             
+            
+            
+
+        }
+        public void ShowInfo() 
+        {
+            Console.WriteLine($"Fullname :{FullName}" + $"Email : {Email}");
+        }
+
+       
+    }
+}
